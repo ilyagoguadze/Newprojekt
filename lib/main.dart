319d52e6380f881114a9e9b5419ff4062fba0bb1
ilyagoguadze <74207653+ilyagoguadze@users.noreply.github.com>
2020-11-11 10:40:43 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,22 @@
 import 'package:flutter/material.dart';
-
-import '2.dart';
-
+import 'package:flutternew_project/2.dart';
 
 class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Приложение от Алихана'),
+      appBar: AppBar(title: Text('Илья production'),
         centerTitle: true,
-        backgroundColor: Colors.black,
+        backgroundColor: Colors.red,
       ),
       body: Center(
-
           child: RaisedButton(
-            onPressed: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => SecondScreen()));
-            },
-            child: Text('Открой послание))', style: TextStyle(color: Colors.white),),
-            color: Colors.black,
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => SecondScreen()));
+              },
 
-          )),
-      backgroundColor: Colors.white12,
+              child: Text('Ну давай залезай сюда '))),
     );
   }
 }
