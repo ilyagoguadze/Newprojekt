7e5c6f8512c7260df39c680b25ec1aaaad7de1c4
ilyagoguadze <74207653+ilyagoguadze@users.noreply.github.com>
2020-11-11 10:41:20 +0300

    Update 2.dart

--- lib/2.dart
+++ lib/2.dart
@@ -2,23 +2,26 @@ import 'package:flutter/material.dart';
 
 class SecondScreen extends StatelessWidget {
   @override
+
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Приложение от Алихана'),
-        centerTitle: true,
-        backgroundColor: Colors.black,
-      ),
-      body: Column(
+        appBar: AppBar(
+          title: Text('Илья production'),
+          backgroundColor: Colors.black,
+        ),
+        body:
+        Column(
 
-        children: <Widget>[
-          Text('Передаю привет Flutter - разработчикам.', style: TextStyle(color: Colors.red, fontSize: 35.0),),
-          Image.network('https://adukar.by/images/photo/programmist-razrabotchik.jpg'),
+          children: <Widget>[
+            Image.network('https://img11.postila.ru/data/f0/70/3a/2e/f0703a2e49c4025e608a9ac40b8e57129606cf42a08d48b4578ac02e4ef2c56d.jpg'),
+            Text('Без труда не вытащить рыбку из пруда',
+              style: TextStyle( // а у текста появился виджет, который его стилизует
+                  fontSize : 31, color: Colors.red),)
+          ],
+        ),
+        backgroundColor: Colors.black
 
-
-        ],
-      ),
-      backgroundColor: Colors.white12,
     );
+
   }
 }
